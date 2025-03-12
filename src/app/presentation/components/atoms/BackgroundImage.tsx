2d7d4b99d8db10f0158fc8bef3115ca189740f54
Franco Ghilardi <86130361+FrancoGhilardi@users.noreply.github.com>
2025-03-11 23:36:03 -0300

    Se crea componente BackgroundImage (#5)

--- src/app/presentation/components/atoms/BackgroundImage.tsx
+++ src/app/presentation/components/atoms/BackgroundImage.tsx
@@ -1,1 +1,23 @@
-// Imagen de fondo
+import React from "react";
+
+interface Props {
+  src?: string;
+  opacity?: number;
+}
+
+const BackgroundImage: React.FC<Props> = ({
+  src = "/images/background.png",
+  opacity = 0.5,
+}) => {
+  return (
+    <div
+      className="absolute inset-0 bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${src})`,
+        opacity,
+      }}
+    />
+  );
+};
+
+export default BackgroundImage;
